fc9cf6750a106386847b7024dfa51ccc752d2523
Moshe Mizrachi <moshepremire@gmail.com>
2024-12-12 15:02:34 +0200

    Add start app files

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,38 +1,17 @@
 package main
 
 import (
-	"book_service/pkg/clients"
-	"book_service/pkg/routes"
+	app "book_service/pkg"
 	"book_service/pkg/utils"
-	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
-	"github.com/joho/godotenv"
-	"log"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Printf("Error loading .env file: %v", err)
-	}
+	app.SetupEnv()
+	app.InitClients()
 
-	if err := clients.InitElasticsearchClient(); err != nil {
-		log.Fatalf("Failed to initialize Elasticsearch: %v", err)
-	}
-	clients.InitWorkerPool(10)
-
-	r := gin.New()
-	binding.EnableDecoderDisallowUnknownFields = true
-
-	r.Use(gin.Recovery())
-	r.Use(utils.CustomLogger())
-
-	routes.RegisterRoutes(r)
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run("0.0.0.0:1234") // listen and serve on 0.0.0.0:8080
+	server := app.SetupServer()
+	port, _ := utils.GetEnvVar[string]("PORT", "8080")
+	logrus.Infof("Running server on :%s", port)
+	server.Run("0.0.0.0:" + port)
 }
---
+++ pkg/app.go
@@ -0,0 +1,48 @@
+package pkg
+
+import (
+	"book_service/pkg/clients"
+	mw "book_service/pkg/middlewares"
+	"book_service/pkg/routes"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+	"os"
+)
+
+func SetupEnv() {
+	if os.Getenv("GIN_MODE") == "test" {
+		if err := godotenv.Load(".env.test"); err != nil {
+			logrus.Infof("Error loading .env.test file: %v", err)
+		}
+	} else {
+		if err := godotenv.Load(); err != nil {
+			logrus.Infof("Error loading .env file: %v", err)
+		}
+	}
+}
+
+func SetupServer() *gin.Engine {
+	app := gin.New()
+	binding.EnableDecoderDisallowUnknownFields = true
+
+	logrus.Infof("Setting up middlewares")
+	app.Use(gin.Recovery())
+	app.Use(mw.Logger())
+	app.Use(mw.RecordActions())
+
+	routes.RegisterRoutes(app)
+	logrus.Infof("Middlewares and routes initialized")
+
+	return app
+}
+
+func InitClients() {
+	if err := clients.InitElasticsearchClient(); err != nil {
+		logrus.Infof("Failed to initialize Elasticsearch: %v", err)
+	}
+
+	clients.InitRedisClient()
+	clients.InitWorkerPool(10)
+}
